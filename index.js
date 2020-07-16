d98fe534671b4a0daf10512858f2d29d55f6298b
Deeswoc <47671781+Deeswoc@users.noreply.github.com>
2020-07-15 20:08:21 -0500

    added the request function to callSendAPI

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (body.object === 'page') {
       handlePostback(sender_psid, webhook_event.postback);
     }
   });
-    
+
     // Returns a '200 OK' response to all requests
   res.status(200).send('EVENT_RECEIVED');
     
@@ -103,6 +103,18 @@ app.get('/webhook', (req, res) => {
         },
         "message": response
       }
-    
+      // Send the HTTP request to the Messenger Platform
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
   }
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
